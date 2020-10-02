357cf0fbd93dbc0fb85ef882f7555ab0e1fa5c4d
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-02 15:05:01 +0200

    Variabile parola

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  $word = "Caso";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>La parola inserita è: <?php echo $word ?></h1>
+  </body>
+</html>
