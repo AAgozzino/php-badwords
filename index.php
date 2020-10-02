0f1697567b28dabb15b220270435bef4dff2e707
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-02 15:10:51 +0200

    Lunghezza parola

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   // Variabile parola
   $word = $_GET["word"];
+  $length = strlen($word);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -10,5 +11,6 @@
   </head>
   <body>
     <h1>La parola inserita è: <?php echo $word ?></h1>
+    <h2>Lunghezza parola: <?php echo $length ?></h2>
   </body>
 </html>
